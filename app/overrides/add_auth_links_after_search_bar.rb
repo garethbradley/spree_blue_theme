3cf4833d07bd991b47f75c241b2a73fa61f7c985
Gareth Bradley <gb@garethbradley.co.uk>
2013-01-15 22:39:13 +0000

    Styling update

--- app/overrides/add_auth_links_after_search_bar.rb
+++ app/overrides/add_auth_links_after_search_bar.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => "spree/ff_shared/_site_header",
+Deface::Override.new(:virtual_path => "ff_shared/_site_header",
                      :name => "auth_shared_login_bar",
                      :insert_before => "li#home-link",
                      :partial => "spree/shared/login_bar",
