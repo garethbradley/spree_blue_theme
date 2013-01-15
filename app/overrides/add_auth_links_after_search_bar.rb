06fb29e7cee0f65c78937bd5b5392164baf1fe3d
Gareth Bradley <gb@garethbradley.co.uk>
2013-01-15 22:36:28 +0000

    Styling update

--- app/overrides/add_auth_links_after_search_bar.rb
+++ app/overrides/add_auth_links_after_search_bar.rb
@@ -1,7 +1,7 @@
-Deface::Override.new(:virtual_path => "spree/shared/_nav_bar",
+Deface::Override.new(:virtual_path => "spree/ff_shared/_site_header",
                      :name => "auth_shared_login_bar",
                      :insert_before => "li#home-link",
                      :partial => "spree/shared/login_bar",
-                     :disabled => false, 
-                     :sequence => { :after => 'add_store_menu_to_top_header' }
+                     :disabled => false
                     )
+
