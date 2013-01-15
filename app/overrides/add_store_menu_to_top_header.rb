84ba3d2ef38607977dbc7fa9e777294bc57af2ea
Gareth Bradley <gb@garethbradley.co.uk>
2013-01-15 20:22:07 +0000

    Fixed header

--- app/overrides/add_store_menu_to_top_header.rb
+++ app/overrides/add_store_menu_to_top_header.rb
@@ -1,4 +1,5 @@
 Deface::Override.new(:virtual_path => 'spree/shared/_nav_bar',
                      :name => 'add_store_menu_to_top_header',
                      :insert_after => 'li#search-bar',
-                     :partial => 'spree/shared/store_menu')
+                     :partial => 'spree/shared/main_nav_bar')
+
