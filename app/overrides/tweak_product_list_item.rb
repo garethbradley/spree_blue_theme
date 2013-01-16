e18faa477a462c09d5185851af19f068de8785d2
Gareth Bradley <gb@garethbradley.co.uk>
2013-01-16 19:14:14 +0000

    Styling update

--- app/overrides/tweak_product_list_item.rb
+++ app/overrides/tweak_product_list_item.rb
@@ -1,4 +1,5 @@
 Deface::Override.new(:virtual_path => 'spree/shared/_products',
+                     :disabled => true,
                      :name => 'tweak_product_list_item',
                      :replace => '[data-hook="products_list_item"]',
                      :text => %q{ <li id="product_<%= product.id %>" data-hook="products_list_item">
@@ -8,3 +9,4 @@ Deface::Override.new(:virtual_path => 'spree/shared/_products',
                                     <% end %>
                                     <%= link_to small_image(product), product %>
                                   </li> })
+
